fd2cf9ef7c03b189e060f51c57962bc1cdc8c531
Davi Vasconcelos <vdrdavi@gmail.com>
2021-03-13 17:25:07 -0300

    Simplificando opcionalidade do hifen

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -14,7 +14,7 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            string padrao = "[0-9]{4,5}-{0,1}[0-9]{4}]";
+            string padrao = "[0-9]{4,5}-?[0-9]{4}";
             string texto = "Meu número é: 4324-2437";
             Match match = Regex.Match(texto, padrao);
             Console.WriteLine(match);

11c07e595011f1e23c5ed5b96c75ed66b063de9e
Davi Vasconcelos <vdrdavi@gmail.com>
2021-04-07 16:13:45 -0300

    Usando a classe List

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -14,27 +14,18 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            Lista<int> idades = new Lista<int>();
-
-            idades.AdicionarVarios(63, 15, 21, 50);
-            idades.Remover(15);
-            for (int i = 0; i < idades.Tamanho; i++)
+            List<int> idades = new List<int>();
+            idades.Add(1);
+            idades.Add(12);
+            idades.Add(72);
+            idades.Add(45);
+            idades.Add(24);
+            idades.Add(21);
+            idades.Add(100);
+            idades.Remove(15);
+            for (int i = 0; i < idades.Count; i++)
             {
-                Console.WriteLine(idades.GetItemNoindice(i));
-            }
-
-            Lista<string> cursos = new Lista<string>();
-            cursos.AdicionarVarios("C# Parte 1", "C# Parte 2", "C# Parte 3");
-            for (int i = 0; i < cursos.Tamanho; i++)
-            {
-                Console.WriteLine(cursos.GetItemNoindice(i));
-            }
-
-            Lista<ContaCorrente> contas = new Lista<ContaCorrente>();
-            contas.AdicionarVarios(new ContaCorrente(124, 54354), new ContaCorrente(201, 44354));
-            for (int i = 0; i < contas.Tamanho; i++)
-            {
-                Console.WriteLine(contas.GetItemNoindice(i));
+                Console.WriteLine(idades[i]);
             }
 
             Console.ReadLine();
